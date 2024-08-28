b71bb73df1a4a33c63c7b4d5b4064ba542e8dc83
adele <akak4456@naver.com>
2024-08-28 15:23:29 +0900

    [INIT] first commit

---
+++ src/main/java/com/gntour/gangneungyeojido/GangneungYeojidoApplication.java
@@ -0,0 +1,13 @@
+package com.gntour.gangneungyeojido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangneungYeojidoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GangneungYeojidoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gntour/gangneungyeojido/GangneungYeojidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gntour.gangneungyeojido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GangneungYeojidoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
