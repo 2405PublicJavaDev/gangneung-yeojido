aae8c404b94a0f181ed81ce78326224f4ed379bf
adele <akak4456@naver.com>
2024-08-28 17:47:30 +0900

    [ADD] 조승효 담당한 영역 클래스 다이어그램에 있는 클래스 작성함

---
+++ src/main/java/com/gntour/gangneungyeojido/app/main/MainController.java
@@ -0,0 +1,16 @@
+package com.gntour.gangneungyeojido.app.main;
+
+public class MainController {
+    public void showMainPage() {
+
+    }
+    public void showAddReqMarkerPage() {
+
+    }
+    public void getMarkerOutline() {
+
+    }
+    public void addReqMarker() {
+
+    }
+}
---
+++ src/main/java/com/gntour/gangneungyeojido/domain/travel/vo/ReqMarkAdd.java
@@ -0,0 +1,19 @@
+package com.gntour.gangneungyeojido.domain.travel.vo;
+
+import java.sql.Timestamp;
+
+public class ReqMarkAdd {
+    private Long reqMarkAddNo;
+    private String travelName;
+    private String address;
+    private String phone;
+    private Timestamp startTime;
+    private Timestamp endTime;
+    private Long parkFee;
+    private Long entryFee;
+    private Long useFee;
+    private String introduce;
+    private String acceptableStatus;
+    private String siteUrl;
+    private String imageUrl;
+}
