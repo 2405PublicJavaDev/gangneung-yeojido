f6b8a82ce973ac27771a024cae7fa8b4d33ab4ed
adele <akak4456@naver.com>
2024-08-30 11:46:51 +0900

    [ADD] DDL Query 추가함, TravelCategory, TraveRegion 추가함

---
+++ src/main/java/com/gntour/gangneungyeojido/common/TravelCategory.java
@@ -0,0 +1,28 @@
+package com.gntour.gangneungyeojido.common;
+
+public enum TravelCategory {
+    /**
+     * 산 트래킹
+     */
+    MOUNTAIN,
+    /**
+     * 바다 해안
+     */
+    SEA,
+    /**
+     * 호수 공원
+     */
+    PARK,
+    /**
+     * 역사 유적
+     */
+    HISTORY,
+    /**
+     * 전시
+     */
+    DISPLAY,
+    /**
+     * 레저 체험
+     */
+    LEISURE
+}
---
+++ src/main/java/com/gntour/gangneungyeojido/common/TravelRegion.java
@@ -0,0 +1,24 @@
+package com.gntour.gangneungyeojido.common;
+
+public enum TravelRegion {
+    /**
+     * 주문진권
+     */
+    JUMUNJIN,
+    /**
+     * 경포권
+     */
+    GYEONGPO,
+    /**
+     * 시내권
+     */
+    CITY,
+    /**
+     * 대관령권
+     */
+    DAEGWALLYEONG,
+    /**
+     * 정동진 옥계권
+     */
+    JEONGDONGJIN
+}
