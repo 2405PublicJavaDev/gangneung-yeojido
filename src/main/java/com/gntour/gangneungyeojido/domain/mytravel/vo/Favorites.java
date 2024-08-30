ee596b52b9863caf4e3f6a19301331f49bc2a6ae
ino <ino456852@gmail.com>
2024-08-30 17:46:32 +0900

    [CNG] 어노테이션 추가함

--- src/main/java/com/gntour/gangneungyeojido/domain/mytravel/vo/Favorites.java
+++ src/main/java/com/gntour/gangneungyeojido/domain/mytravel/vo/Favorites.java
@@ -1,7 +1,11 @@
 package com.gntour.gangneungyeojido.domain.mytravel.vo;
 
-import java.sql.Timestamp;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.sql.Timestamp;
+@Getter
+@Setter
 public class Favorites {
     private Long favoritesNo;
     private String memberId;
