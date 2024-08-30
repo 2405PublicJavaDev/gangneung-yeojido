f79b5f3bbdbe66ce3da9b8678e7126a5582ef9cf
TaewoonEom <thtw28@gmail.com>
2024-08-30 17:03:49 +0900

    [CNG] 관리자 - 여행지 목록 html, 상세조회 html 작업

--- src/main/java/com/gntour/gangneungyeojido/app/admin/AdminTravelController.java
+++ src/main/java/com/gntour/gangneungyeojido/app/admin/AdminTravelController.java
@@ -1,21 +1,29 @@
 package com.gntour.gangneungyeojido.app.admin;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
 public class AdminTravelController {
 
     /**
      * 담당자 : 엄태운님
      * 관련기능 : [관리자 기능] 등록된 여행지 리스트 확인
-     * url :
      */
-    public void showAllTravelPage() {
-
+    @GetMapping("/admin/travel-list")
+    public String showAllTravelPage() {
+        log.info("showAllTravelPage");
+        return "admin/travel-list";
     }
 
 
     /**
      * 담당자 : 엄태운님
      * 관련기능 : [관리자 기능] 등록된 여행지 상세 정보 조회, [관리자 기능(페이지 폼)] 등록된 여행지 수정
-     * url :
      */
     public void showTravelDetailPage() {
 
@@ -24,7 +32,6 @@ public class AdminTravelController {
     /**
      * 담당자 : 엄태운님
      * 관련기능 : [관리자 기능] 등록된 여행지 수정
-     * url :
      */
     public void updateTravelInfo() {
 
@@ -33,7 +40,6 @@ public class AdminTravelController {
     /**
      * 담당자 : 엄태운님
      * 관련기능 : [관리자 기능] 등록된 여행지 삭제
-     * url :
      */
     public void removeTravelInfo() {
 
--- src/main/java/com/gntour/gangneungyeojido/app/travel/TravelController.java
+++ src/main/java/com/gntour/gangneungyeojido/app/travel/TravelController.java
@@ -1,5 +1,10 @@
 package com.gntour.gangneungyeojido.app.travel;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
 public class TravelController {
 
     /**
