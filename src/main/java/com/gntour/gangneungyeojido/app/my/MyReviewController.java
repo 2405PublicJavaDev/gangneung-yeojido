9efce65f309a274defd9afa7dec40b04d5750880
ino <ino456852@gmail.com>
2024-09-02 18:07:11 +0900

    [CNG] 09-02 href 연결을 위해 주소 변경

--- src/main/java/com/gntour/gangneungyeojido/app/my/MyReviewController.java
+++ src/main/java/com/gntour/gangneungyeojido/app/my/MyReviewController.java
@@ -1,10 +1,20 @@
 package com.gntour.gangneungyeojido.app.my;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+@RequiredArgsConstructor
+@Slf4j
 public class MyReviewController {
 
     /**
      * 담당자 : 백인호님
      * 관련 기능 : [마이페이지 기능] 나의 리뷰 리스트 조회
      */
-    public void showMyReviewPage() {};
+    @GetMapping("/review")
+    public String showMyReviewPage() {
+        return "/myPage/myReview";
+    };
 }
