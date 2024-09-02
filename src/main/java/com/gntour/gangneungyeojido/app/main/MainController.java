9e067ccac59ba4760538540c2622a843526e476a
adele <akak4456@naver.com>
2024-09-02 11:06:13 +0900

    [CNG] 불필요한 url 주석 삭제함

--- src/main/java/com/gntour/gangneungyeojido/app/main/MainController.java
+++ src/main/java/com/gntour/gangneungyeojido/app/main/MainController.java
@@ -19,7 +19,6 @@ public class MainController {
     /**
      * 담당자: 조승효님
      * 관련기능: [메인 기능] 여행지 마커 리스트 조회, [메인 기능] 이달의 신규 여행지 확인
-     * url:
      */
     @GetMapping("/")
     public String showMainPage(Model model) {
@@ -30,7 +29,6 @@ public class MainController {
     /**
      * 담당자: 조승효님
      * 관련기능: [메인 기능(페이지 폼)] 마커 승인 요청
-     * url:
      */
     public void showAddReqMarkerPage() {
 
@@ -39,7 +37,6 @@ public class MainController {
     /**
      * 담당자: 조승효님
      * 관련기능: [메인 기능] 여행지 마커에 대한 개요 확인
-     * url:
      */
     public void getMarkerOutline() {
 
@@ -48,7 +45,6 @@ public class MainController {
     /**
      * 담당자: 조승효님
      * 관련기능: [메인 기능] 마커 승인 요청
-     * url:
      */
     public void addReqMarker() {
 
