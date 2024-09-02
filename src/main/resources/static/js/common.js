1d291f19be06b43a8faeed25abc301ad9372b806
adele <akak4456@naver.com>
2024-09-03 08:58:49 +0900

    [CNG] window click 시 발생하는 버그 해결함

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,7 +1,6 @@
 let myExpandVisible = false;
 window.addEventListener('click', (e) => {
     if(e.target !== document.querySelector("#header-my") && document.querySelector(".header-my-expand") && !document.querySelector(".header-my-expand").contains(e.target)) {
-        e.preventDefault();
         document.querySelector(".header-my-expand").style.display = "none";
         myExpandVisible = false;
     }
