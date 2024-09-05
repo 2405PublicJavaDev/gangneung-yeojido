12a48eaf34f2992efab46142265c64142db1992a
yoonneeeee <imyk.athena@gmail.com>
2024-09-05 17:03:00 +0900

    [CNG] 주요 공지사항 관련 수정 반영
    - 사용자 노출 공지 : 중요 공지 상단 고정
    - 푸터 주요 공지 3개 노출

--- src/main/java/com/gntour/gangneungyeojido/domain/notice/service/NoticeService.java
+++ src/main/java/com/gntour/gangneungyeojido/domain/notice/service/NoticeService.java
@@ -4,31 +4,36 @@ import com.gntour.gangneungyeojido.app.notice.dto.NoticeSearchCondition;
 import com.gntour.gangneungyeojido.common.Page;
 import com.gntour.gangneungyeojido.domain.notice.vo.Notice;
 
+import java.util.List;
+
 public interface NoticeService {
     /**
      * 공지사항 리스트 Service
-     * @return List<Notice>
-    **/
+     **/
     Page<Notice, NoticeSearchCondition> getAllNotices(Integer currentPage, NoticeSearchCondition condition);
 
-    void getImportantNotices();
+    /**
+     * 중요 공지사항 리스트 Service
+     **/
+    List<Notice> getImportantNotices();
 
     /**
      * 공지사항 상세 페이지 조회 Service
-     * @return Notice
-     */
+     **/
     Notice getDetailNotice(Long noticeNo);
 
     /**
      * 공지사항 등록 Service
-     * @return int
      **/
     int addNotice(Notice notice);
+
+    /**
+     * 공지사항 삭제 Service
+     **/
     void removeNotice();
 
     /**
      * 공지사항 수정 Service
-     * @return int
      **/
     int modifyNotice(Notice notice);
 }
