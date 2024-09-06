413c16fe7f238e2f5b6487afde50ce3ace1274c0
adele <akak4456@naver.com>
2024-09-06 10:06:10 +0900

    [CNG] MainController 에 fragments/footer 가 정상작동하게 함

--- src/main/java/com/gntour/gangneungyeojido/app/main/MainController.java
+++ src/main/java/com/gntour/gangneungyeojido/app/main/MainController.java
@@ -8,6 +8,8 @@ import com.gntour.gangneungyeojido.common.MemberUtils;
 import com.gntour.gangneungyeojido.common.exception.BusinessException;
 import com.gntour.gangneungyeojido.common.exception.EmptyResponse;
 import com.gntour.gangneungyeojido.common.exception.ErrorCode;
+import com.gntour.gangneungyeojido.domain.notice.service.NoticeService;
+import com.gntour.gangneungyeojido.domain.notice.vo.Notice;
 import com.gntour.gangneungyeojido.domain.travel.service.TravelService;
 import com.gntour.gangneungyeojido.domain.travel.vo.TravelInfo;
 import jakarta.servlet.http.HttpSession;
@@ -20,11 +22,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class MainController {
     private final TravelService travelService;
+    private final NoticeService noticeService;
     /**
      * 담당자: 조승효님
      * 관련기능: [메인 기능] 여행지 마커 리스트 조회, [메인 기능] 이달의 신규 여행지 확인
@@ -68,5 +73,16 @@ public class MainController {
             throw new BusinessException(ErrorCode.NO_UPDATE);
         }
         return new EmptyResponse();
+    }
+
+    /**
+     * 담당자 : 김윤경님
+     * 관련 기능 : [푸터 기능] 주요 공지사항 리스트 조회
+     */
+    @ModelAttribute
+    public String getImportantNoticeList(Model model){
+        List<Notice> importantNotices = noticeService.getImportantNotices();
+        model.addAttribute("footerImportantNotices", importantNotices);
+        return "fragments/footer";
     }
 }
