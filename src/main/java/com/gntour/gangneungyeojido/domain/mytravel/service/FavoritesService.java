df2ff3c12fe9cea6aa12a543921d35bf6515f189
adele <akak4456@naver.com>
2024-09-10 10:04:52 +0900

    [CNG] 여행지 리스트 즐겨찾기 추가, 해제 구현함

--- src/main/java/com/gntour/gangneungyeojido/domain/mytravel/service/FavoritesService.java
+++ src/main/java/com/gntour/gangneungyeojido/domain/mytravel/service/FavoritesService.java
@@ -1,5 +1,6 @@
 package com.gntour.gangneungyeojido.domain.mytravel.service;
 
+import com.gntour.gangneungyeojido.app.my.dto.AddFavoriteResponse;
 import com.gntour.gangneungyeojido.app.my.dto.FavoritesResponse;
 import com.gntour.gangneungyeojido.app.my.dto.FavoritesSearchCondition;
 import com.gntour.gangneungyeojido.common.Page;
@@ -10,7 +11,7 @@ public interface FavoritesService {
 
     Page<FavoritesResponse, FavoritesSearchCondition> getAllFavoritesByMember(Integer currentPage, FavoritesSearchCondition searchCondition);
 
-    int addFavorite(String memberId, Long travelNo);
+    AddFavoriteResponse addFavorite(String memberId, Long travelNo);
 
     int removeFavorite(String memberId, Long favoritesNo);
 }
